b523e13071e765a0d9bf7e46136ffcf221f4deb6
unknown <edward2000ed@gmail.com>
2021-04-30 15:01:41 +0530

    finished pendulum

--- sketch.js
+++ sketch.js
@@ -1,14 +1,22 @@
 
-let bob, len, angle, origin;
+let bob;
+let len;
+let angle;
+let origin;
+let angleA;
+let angleV;
+let gravity;
 
 function setup() {
 
     createCanvas(600, 400);
 
     angle = PI / 4;
+    angleV = 0;
+    gravity = 1;
     bob = createVector();
     origin = createVector(300, 0);
-    len = 300;
+    len = 200;
    
 
 }
@@ -17,13 +25,21 @@ function draw() {
 
     background(20);
 
-    bob.x = len * cos(angle) + origin.x;
-    bob.y = len * sin(angle) + origin.y;
+    let force = gravity * sin(angle);
+    
+    angleA = -1 * force / len;
+    angleV += angleA;
+    angle += angleV;
+    
+    bob.x = len * sin(angle) + origin.x;
+    bob.y = len * cos(angle) + origin.y;
 
     stroke(255);
     strokeWeight(6);
     fill(40);
     line(origin.x, origin.y , bob.x, bob.y);
-    circle(bob.x, bob.y ,60);
+    circle(bob.x, bob.y ,64);
+
+    // noLoop();
 
 }
