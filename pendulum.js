a17a50b516c8dffb54766cddf605552e88359e4c
unknown <edward2000ed@gmail.com>
2021-05-02 20:48:00 +0530

    added my touch

--- pendulum.js
+++ pendulum.js
@@ -1,29 +1,35 @@
 class Pendulum {
 
-    constructor(len, size) {
+    constructor(len, size, angle, gravity, colour) {
         this.len = len;
         this.size = size;
-        this.x = this.len * sin(angle) + origin.x;
-        this.y = this.len * cos(angle) + origin.y;
+        this.angle = angle;
+        this.gravity = gravity;
+        this.angleV = 0;
+        this.angleA;
+        this.colour = colour;
+        this.x = this.len * sin(this.angle) + origin.x;
+        this.y = this.len * cos(this.angle) + origin.y;
     }
 
     show() {
-        stroke(255);
-        strokeWeight(6);
-        fill(40);
+        stroke(this.colour, 20);
+        // noStroke();
+        // strokeWeight(4);
+        fill(this.colour, 10);
         line(origin.x, origin.y, this.x, this.y);
         ellipse(this.x, this.y ,this.size);
 
     }
 
     update() {
-        let force = gravity * sin(angle);
+        let force = this.gravity * sin(this.angle);
     
-        angleA = -1 * force / this.len;
-        angleV += angleA;
-        angle += angleV;
+        this.angleA = -1 * force / this.len;
+        this.angleV += this.angleA;
+        this.angle += this.angleV;
         
-        this.x = this.len * sin(angle) + origin.x;
-        this.y = this.len * cos(angle) + origin.y;
+        this.x = this.len * sin(this.angle) + origin.x;
+        this.y = this.len * cos(this.angle) + origin.y;
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,49 +1,49 @@
 
-let bob;
-// let len;
-let angle;
+let angleUp;
+let angleDown;
 let origin;
-let angleA;
-let angleV;
-let gravity;
-let pend;
+let gravityUp;
+let gravityDown;
+let pendulums = [];
+let sizing;
+let colorUp;
+let colorDown;
+
 
 function setup() {
 
-    createCanvas(600, 400);
+    createCanvas(windowWidth, windowHeight);
 
-    angle = PI / 3;
-    angleV = 0;
-    gravity = 1;
-    bob = createVector();
-    origin = createVector(300, 200);
-    // len = 150;
-    pend = new Pendulum(150, 60)
+    angleUp = 2 * PI - .01 ;
+    angleDown = PI - .01 ;
+    
+    gravityUp =  - 1;
+    gravityDown = 1;
 
+    sizing = 10;
+
+    colorUp = color(247, 187, 74);
+    colorDown = color(109, 235, 87);
+
+    origin = createVector(width / 2, height / 2);
+  
+    for(let i = 0; i < height + 500; i += sizing) {
+        if(random() < .5)
+            pendulums.push(new Pendulum(i + sizing, sizing, angleUp, gravityUp, colorUp));
+        else
+            pendulums.push(new Pendulum(i + sizing, sizing, angleDown, gravityDown, colorDown));
+    }
 }
 
 function draw() {
 
-    background(20);
+    background(78, 39, 97, 20);
 
-    // let force = gravity * sin(angle);
     
-    // angleA = -1 * force / len;
-    // angleV += angleA;
-    // angle += angleV;
-    
-    // bob.x = len * sin(angle) + origin.x;
-    // bob.y = len * cos(angle) + origin.y;
-
-    // stroke(255);
-    // strokeWeight(6);
-    // fill(40);
-    // line(origin.x, origin.y , bob.x, bob.y);
-    // ellipse(bob.x, bob.y ,60);
-
-    pend.show();
-    pend.update();
-
-    // noLoop();
+    pendulums.forEach(pendulum => {
+       pendulum.show();
+       pendulum.update(); 
+    });
+   
 
 }
